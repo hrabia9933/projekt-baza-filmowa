e479900c88a33cbf19c7a39151eba9283181c240
Artur Porębski <A.Porebski@wasko.pl>
2020-08-04 08:44:24 +0200

    dodany pusty projekt

---
+++ src/main/java/com/example/filmo/FilmowyProjektApplication.java
@@ -0,0 +1,13 @@
+package com.example.filmo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmowyProjektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmowyProjektApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/filmo/FilmowyProjektApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.filmo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmowyProjektApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
